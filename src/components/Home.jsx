e3dad9d298fe71f7bb8bdb8534635e547610954c
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-09-24 13:54:40 -0600

    another commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,12 +7,12 @@ const Home = () => {
     <>
       <div className="container home">
         <div className="left">
-          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur deleniti neque distinctio molestias perferendis cum.</h2>
+          <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
           <a href={pdf} download="Marcelo Reyna Rangel.pdf" className="btn btn-outline-warning">Download Resume</a>
           </div>
          <div className="right">
           <div className="img">
-          <img src={`/assets/${hero.imgSrc}`} alt="hero" width="400px" height="400px" />                
+          <img src={`/assets/${hero.imgSrc}`} alt="hero" />                
           </div>
         
         </div>
