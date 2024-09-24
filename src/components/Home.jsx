8cf849deb599e4cf81cdcc8e0a359e9d66851c1e
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-09-24 10:42:47 -0600

    new commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import pdf from '../pdf/Marcelo Reyna Rangel.pdf';
-import hero from './Data/hero.json';
-
+import hero from './data/hero.json';
 
 const Home = () => {
   return (
     <>
-    <div className="container home">
+      <div className="container home">
         <div className="left">
-            <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur deleniti neque distinctio molestias perferendis cum.</h2>
-            <a href={pdf} download="Marcelo Reyna Rangel.pdf" className="btn btn-outline-warning">Download Resume</a>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur deleniti neque distinctio molestias perferendis cum.</h2>
+          <a href={pdf} download="Marcelo Reyna Rangel.pdf" className="btn btn-outline-warning">Download Resume</a>
+          </div>
+         <div className="right">
+          <div className="img">
+          <img src={`/assets/${hero.imgSrc}`} alt="hero" width="400px" height="400px" />                
+          </div>
+        
         </div>
-        <div className="right">
-            <div className="  ">
-               
-            </div>
-        </div>
-    </div>
-    </>
+      </div>
+    </> 
   );
 };
 
