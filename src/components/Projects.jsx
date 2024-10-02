932c2dfa84d15461fb25755c5febc758ecd33f53
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-02 09:13:39 -0600

    other commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+  import React from 'react'
+  import project from './data/projects.json'
 
 
-const Projects = () => {
-  return (
-    <>
-    <div className="container projects">
+  const Projects = () => {
+    return (
+      <>
+      <div className="container projects my-3">
 
-      <h1>PROJECTS</h1>
-    </div>
-    </>
-  )
-}
+        <h1>PROJECTS</h1>
+        <div className="row">
+          {project.map((data)=>(<>
+          <div key={data.id}>
+          <div className="card" style={{width: '18rem'}}>
+  <div className="card-body">
+  <img src="..." className="card-img-top" alt="..."/>
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+          </div>
+          </>))}
+        </div>
 
-export default Projects
+      </div>
+      </>
+    )
+  }
+
+  export default Projects
 
