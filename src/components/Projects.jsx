d9e470be49659fe999a139f9a16e32190d4645ab
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-02 09:28:20 -0600

    card commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,11 +11,11 @@
         <div className="row">
           {project.map((data)=>(<>
           <div key={data.id}>
-          <div className="card" style={{width: '18rem'}}>
+          <div className="card" style={{width:'18rem'}}>
   <div className="card-body">
   <img src="..." className="card-img-top" alt="..."/>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 className="card-title">Card Title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
     <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
