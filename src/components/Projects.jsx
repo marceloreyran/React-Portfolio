72834e3650cc9c63d41c3bff484842bab0972a49
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-04 14:14:26 -0600

    card add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,8 +12,14 @@
           {project.map((data)=>(<>
           <div key={data.id} className='my-3 col-sm-6 col-md-4 col-lg-3 mx-3'>
           <div className="card bg-dark text-light" style={{width:'18rem'}}>
+            <div className="img">
+            <img src={data.imageSrc} className="card-img-top" alt="..."
+            style={{
+              width: '250px',
+              height: '250px',
+            }}/>
+            </div>
   <div className="card-body text-center">
-  <img src={data.imageSrc} className="card-img-top" alt="..."/>
     <h5 className="card-title">{data.title}</h5>
     <p className="card-text">{data.description}</p>
     <a href="#" className="btn btn-primary ">Go Somewhere</a>
