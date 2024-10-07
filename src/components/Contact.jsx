7e1c1647f86e856ed3bc56b37602d2f1db541440
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-07 13:33:43 -0600

    icon size

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+
 
 const Contact = () => {
     return(
-        <div>Contact</div>
+        <>
+        <div className="container contact">
+            <h1>CONTACT ME </h1>
+            <div className="contact-icon">
+                <div className="items">
+                <FaInstagramSquare size={80} />
+                </div>
+                <div className="items">
+                <FaLinkedin size={80}/>
+
+
+                </div>
+                <div className="items"></div>
+                <div className="items"></div>
+                <div className="items"></div>
+            </div>
+        </div>
+        </>
     )
 }
 
