aeada2632af869142f94ae192827e0706bdc99aa
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-07 13:04:05 -0600

    change bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Experience from  './components/Experience';
 import Skills from './components/Skills';
 import Projects  from './components/Projects';
+import Contact from './components/Contact'
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
         <Experience />
         <Skills />
         <Projects />
+        <Contact/>
         </div>
       
         </>
