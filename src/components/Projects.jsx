df6eabd6f554a41e5c48455f627c4854e3bd5527
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-07 12:40:23 -0600

    size card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@
         <h1>PROJECTS</h1>
         <div className="row d-flex justify-content-center align-items-center">
           {project.map((data)=>(<>
-          <div key={data.id} className='my-3 col-sm-6 col-md-4 col-lg-3 mx-3'>
+          <div key={data.id} className='my-4 col-sm-6 col-md-4 col-lg-3 mx-4'>
           <div className="card bg-dark text-light" style={{width:'18rem', border:'1px solid yellow', boxShadow:'5px 5px 10px 0px rgba(101, 175, 10, 0.5)'}}>
             <div className="img d-flex justify-content-center align-items-center p-3">
             <img src={data.imageSrc} className="card-img-top" alt="..."
