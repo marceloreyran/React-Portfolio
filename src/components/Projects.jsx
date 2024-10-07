f7607025f4d899e3d1bdee9fa3c0011a51729347
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-07 12:48:43 -0600

    change demo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,8 @@
   <div className="card-body text-center">
     <h5 className="card-title">{data.title}</h5>
     <p className="card-text">{data.description}</p>
-    <a href="#" className="btn btn-primary ">Go Somewhere</a>
+    <a href={data.source} className="btn btn-primary mx-3 ">Github</a>
+    <a href={data.demo} className='btn btn-warning'>Code</a>
   </div>
 </div>
           </div>
