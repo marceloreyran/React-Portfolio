cd6dd7256a578ce01488b9fbec170baad18898a5
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-08 09:44:07 -0600

    color icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,23 +14,23 @@ const Contact = () => {
         <div className="container contact">
             <h1>CONTACT ME </h1>
             <div className="contact-icon">
-                <a href='https://www.instagram.com/'className="items">
+                <a href='https://www.instagram.com/' target='_blank'  className="items" >
                 <FaInstagramSquare className='icons'/>
                 </a>
-                <a href='https://www.linkedin.com/'className="items">
+                <a href='https://www.linkedin.com/' target='_blank' className="items">
                 <FaLinkedin className='icons'/>
 
 
                 </a>
-                <a href='https://www.github.com/'className="items">
+                <a href='https://www.github.com/' target='_blank' className="items">
                 <FaGithubSquare className='icons' />
 
                 </a>
-                <a href='https://www.x.com/'className="items">
+                <a href='https://www.x.com/' target='_blank' className="items">
                 <FaXTwitter className='icons'/>
 
                 </a>
-                <a href='https://www.gmail.com/'className="items">
+                <a href='https://www.gmail.com/' target='_blank' className="items">
                 <BiLogoGmail className='icons' />
 
                 </a>
