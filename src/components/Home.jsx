25d02654f13389afcaf5e9507c71c94ab0940f4e
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-10-08 11:31:56 -0600

    add h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React,  {useEffect,useRef} from 'react';
 import pdf from '../pdf/Marcelo Reyna Rangel.pdf';
 import hero from './data/hero.json';
+import Typed from 'typed.js';
 
 const Home = () => {
-  return (
+  const typedRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Welcome to mi Portfolio, My name is Marcelo Reyna Rangel , I'm Front End Developer"
+        
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+     loop: true,  
+    };
+
+    const typed = new Typed(typedRef.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+   return (
     <>
       <div className="container home">
         <div className="left">
-          <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
-          <a href={pdf} download="Marcelo Reyna Rangel.pdf" className="btn btn-outline-warning">Download Resume</a>
+          <h1 ref={typedRef}></h1>
+          <a href={pdf} download="Marcelo Reyna Rangel.pdf" className="btn btn-outline-warning my-3">Download CV</a>
           </div>
          <div className="right">
           <div className="img">
           <img src={`/assets/${hero.imgSrc}`} alt="hero" />                
           </div>
-        
         </div>
       </div>
-    </> 
+    </>
   );
-};
-
+}
 export default Home;
+   
+
